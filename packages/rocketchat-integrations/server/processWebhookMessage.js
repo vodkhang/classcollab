4269951cd2f815332ecd07966f67f0b4fbe754c1
Khang Vo <vodkhang@gmail.com>
2018-03-28 16:04:25 +1100

    Finish returning message posts for multiple channels

--- packages/rocketchat-integrations/server/processWebhookMessage.js
+++ packages/rocketchat-integrations/server/processWebhookMessage.js
@@ -4,7 +4,6 @@ import s from 'underscore.string';
 this.processWebhookMessage = function(messageObj, user, defaultValues = { channel: '', alias: '', avatar: '', emoji: '' }, mustBeJoined = false) {
 	const sentData = [];
 	const channels = [].concat(messageObj.channel || messageObj.roomId || defaultValues.channel);
-	console.log(channels);
 	for (const channel of channels) {
 		const channelType = channel[0];
 
@@ -82,5 +81,6 @@ this.processWebhookMessage = function(messageObj, user, defaultValues = { channe
 		sentData.push({ channel, message: messageReturn });
 	}
 
+	console.log('sentData', sentData);
 	return sentData;
 };
