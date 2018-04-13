571a51a12cfa4be081131e15fcc34105349326be
entropi <phqtuyen@gmail.com>
2018-04-13 13:15:42 +1000

    disable action link after submitted

--- packages/rocketchat-api/server/v1/chat.js
+++ packages/rocketchat-api/server/v1/chat.js
@@ -293,6 +293,7 @@ RocketChat.actionLinks.register('call_third_party_action', function(message, par
 	options['username'] = Meteor.user().username;	//console.log('action', action);
 	options['email'] = Meteor.user().emails[0]['address'];
 	options['name'] = Meteor.user().name;
+	//console.log('user info: ', Meteor.user());
 	let response = {};
 	if (method.toUpperCase() === 'POST') {
 		response = HTTP.post(action, {
@@ -304,10 +305,30 @@ RocketChat.actionLinks.register('call_third_party_action', function(message, par
 	}
 
 	const deleteAfterSuccess = 'delete_after_success'
+	const originalMessage = RocketChat.models.Messages.findOneById(message._id, {
+		fields: {
+			u: 1,
+			rid: 1,
+			file: 1,
+			ts: 1
+		}
+	});	
 	if (response.statusCode === 200) {
 		if (options[deleteAfterSuccess] === true) {
-			console.log('deleting');
-			Meteor.call('deleteMessage', { _id: message._id });
+			//console.log('deleting');
+			//console.log('message id: ', message._id);
+			//console.log('message content ', message);
+			//RocketChat.deleteMessage(originalMessage, Meteor.user());
+			if (Meteor.user().roles.includes('admin'))
+				Meteor.call('deleteMessage', { _id: message._id });		
+			else {
+				//console.log('user is not an admin');
+				message.actionLinks = null;
+				RocketChat.models.Messages.update(message._id, {actionLinks: null});
+				const m = RocketChat.models.Messages.findOneById(message._id, {fields : {actionLinks : 1}});
+				//console.log('message after delete actionLinks: ', m);
+				RocketChat.updateMessage(message, Meteor.user());
+			}
 		}
 	}
 });
